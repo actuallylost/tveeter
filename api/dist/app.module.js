af0f9e2ed001d5bf1f77ad2f8e9bec6e2150511a
Lost <pandagaminggr@gmail.com>
2023-04-01 15:00:44 +0300

    initial commit

---
+++ api/dist/app.module.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+class AppModule {}
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    return <div className="App"></div>;
+};
+
+export default App;
