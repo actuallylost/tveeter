4333681c85a6b1fa1c2f1adb682d6a364b654e98
Kaylen Dart <actuallyori@gmail.com>
2023-04-11 23:19:38 +0100

    fix: vite entrypoint; formatting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "*.{css,scss,md,json}": "prettier --write",
-    "*.{ts,js}x?": ["prettier --write", "eslint --fix"],
+	"*.{css,scss,md,json}": "prettier --write",
+	"*.{ts,js}x?": ["prettier --write", "eslint --fix"],
 };
