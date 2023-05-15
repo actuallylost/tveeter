227e5270eb2952030831570c2ee8218da092c472
Lost <pandagaminggr@gmail.com>
2023-05-15 19:54:01 +0300

    feat: fix implement supabase auth; implement configservice

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,6 @@
+// NestJS Imports
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 // Modules
 import { SupabaseModule } from "./common/supabase";
@@ -7,9 +9,14 @@ import { MessagesModule } from "./messages";
 import { EventsModule } from "./events";
 import { UsersModule } from "./users";
 import { AuthModule } from "./auth";
+import { configuration } from "./config";
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			load: [configuration],
+		}),
 		UsersModule,
 		MessagesModule,
 		ChannelsModule,
--- api/src/common/supabase/supabase.module.ts
+++ api/src/common/supabase/supabase.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
-import { SupabaseStrategy } from "./supabase.strategy";
 import { SupabaseGuard } from "./supabase.guard";
-import { SupabaseService } from "./supabase.service";
-import { SupaClient } from "./supaclient";
+import SupabaseService from "./supabase.service";
 
 @Module({
-	providers: [SupabaseStrategy, SupabaseGuard, SupabaseService, SupaClient],
-	exports: [SupabaseStrategy, SupabaseGuard, SupabaseService, SupaClient],
+	imports: [ConfigModule],
+	providers: [SupabaseGuard, SupabaseService],
+	exports: [SupabaseGuard, SupabaseService],
 })
 export class SupabaseModule {}
