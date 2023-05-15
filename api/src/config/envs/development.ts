5fb0d58bf3b388d7a32caf35bf55c85bbb6feb83
Lost <pandagaminggr@gmail.com>
2023-05-15 20:31:17 +0300

    fix: environment config files

--- api/src/config/envs/development.ts
+++ api/src/config/envs/development.ts
@@ -1,11 +1,1 @@
-export const config = {
-	db: {
-		dbProvider: process.env.DATABASE_PROVIDER,
-		dbUrl: process.env.DATABASE_DEV_URL,
-	},
-	supabase: {
-		apiUrl: process.env.SUPABASE_URL,
-		apiKey: process.env.SUPABASE_API_KEY,
-		jwtSecret: process.env.SUPABASE_JWT_SECRET,
-	},
-};
+export const config = {};
